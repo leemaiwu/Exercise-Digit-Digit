72215e844d994a7e216b90b367260d7536ca68fa
Laura Wu <leemaiwu@gmail.com>
2023-06-08 18:23:03 -0600

    Completed with toString split and join method

--- solution.js
+++ solution.js
@@ -1,3 +1,9 @@
-function squareDigits(num){
-  return 0;
+const squareDigits = num => {
+    let numArr = num.toString().split('')
+    for (let i = 0; i < numArr.length; i++) {
+        numArr[i] = numArr[i] * numArr[i]
+    }
+    return +numArr.join('')
 }
+
+console.log(squareDigits(9119)) // 811181
